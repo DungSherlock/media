237834cc5beff0e74f230d01050ee5d2915c18a3
DungSherlock <dungsherlock@gmail.com>
2020-12-31 10:16:32 +0700

    nodejs-npm.jpg

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,12 @@
 # This here is waiting for the time when minify starts working properly
 # minify -r -v --match=\.*ml ~/sites/saimiri.io/public
 #find public/ -type f \( -name '*.html' -o -name '*.js' -o -name '*.css' -o -name '*.xml' -o -name '*.svg' \) -exec gzip -v -k -f --best  \;
+#https://ryanstutorials.net/bash-scripting-tutorial/bash-input.php
+# $CI_BUILD_ID
+
+read -p 'Noi dung commit: ' commitVar
 git add -A .
-git commit -a -m "Build $CI_BUILD_ID from GitLab-CI" --allow-empty
+git commit -m """$commitVar""" --allow-empty
 git push
 echo "Đã deploy xong!"
 exit
